04281e8caa4418d7671a4d55238ff36451f396c1
hyunsung <hslgood@gmail.com>
2020-01-15 15:08:39 +0900

    add Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import MainTop from './MainTop';
-import MainNav from './MainNav';
+import BucketList from './BucketList';
+import TodayBucketList from './TodayBucketList';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // fake data
+      bucketList: [
+        { id: 0, userName: '임현성', title: 'test', like: 5 },
+        { id: 1, userName: '임현성', title: 'test', like: 5 },
+        { id: 2, userName: '임현성', title: 'test', like: 5 },
+      ],
+      // bucketList: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/buckets/home', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({ bucketList: result.bucketList });
+      });
+  }
+
   render() {
+    const { bucketList } = this.state;
+    if (!bucketList) {
+      return <div>로딩중</div>;
+    }
     return (
       <div>
-        test <MainTop />
-        <MainNav />
+        <MainTop />
+        <BucketList bucketList={bucketList} />
+        <TodayBucketList />
       </div>
     );
   }
