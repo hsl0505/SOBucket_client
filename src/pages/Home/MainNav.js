8c89e12cd2821e4b7139201e1aae0d37257f8537
hyunsung <hslgood@gmail.com>
2020-01-15 12:43:57 +0900

    add login, signup btn by antd

--- src/pages/Home/MainNav.js
+++ src/pages/Home/MainNav.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
 
 export default class MainNav extends Component {
   render() {
-    return <div>로그인 회원가입</div>;
+    return (
+      <div>
+        <Button className="mainNav_loginBtn" type="primary" size="large">
+          Log In
+        </Button>
+        <Button className="mainNav_signupBtn" type="primary" size="large">
+          Sign Up
+        </Button>
+      </div>
+    );
   }
 }
