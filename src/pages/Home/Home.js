37fff23229f962a96f8fffd7d544f63032c96047
hyunsung <hslgood@gmail.com>
2020-01-16 13:26:05 +0900

    add state(for loading_skelton)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,7 @@ export default class Home extends Component {
     super();
     this.state = {
       // bucketList: null,
+      bucketListLoad: true, // true는 로딩상태
       // fake data
       bucketList: [
         { id: 0, userName: '임현성', title: 'test', like: 5 },
@@ -15,6 +16,7 @@ export default class Home extends Component {
         { id: 2, userName: '임현성', title: 'test', like: 5 },
       ],
       // todayBucketList : null,
+      todayBucketListLoad: true, // true는 로딩상태
       // fake data
       todayBucketList: [
         { id: 0, userName: '임현성', title: 'test', like: 5 },
@@ -30,7 +32,7 @@ export default class Home extends Component {
     })
       .then(res => res.json())
       .then(result => {
-        this.setState({ bucketList: result.bucketList });
+        this.setState({ bucketListLoad: false, bucketList: result.bucketList });
       });
 
     fetch('http://localhost:3001/buckets/home/today', {
@@ -38,20 +40,29 @@ export default class Home extends Component {
     })
       .then(res => res.json())
       .then(result => {
-        this.setState({ todayBucketList: result.todayBucketList });
+        this.setState({
+          todayBucketListLoad: false,
+          todayBucketList: result.todayBucketList,
+        });
       });
   }
 
   render() {
-    const { bucketList, todayBucketList } = this.state;
-    if (!bucketList) {
-      return <div>로딩중</div>;
-    }
+    const {
+      bucketList,
+      todayBucketList,
+      bucketListLoad,
+      todayBucketListLoad,
+    } = this.state;
+
     return (
       <div>
         <MainTop />
-        <BucketList bucketList={bucketList} />
-        <TodayBucketList todayBucketList={todayBucketList} />
+        <BucketList bucketList={bucketList} bucketListLoad={bucketListLoad} />
+        <TodayBucketList
+          todayBucketList={todayBucketList}
+          todayBucketListLoad={todayBucketListLoad}
+        />
       </div>
     );
   }
