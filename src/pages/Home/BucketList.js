d4b85945b1892525d13e46b65aec5d68abee6ed9
hyunsung <hslgood@gmail.com>
2020-01-16 13:26:43 +0900

    add props for loading_skeleton

--- src/pages/Home/BucketList.js
+++ src/pages/Home/BucketList.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 import Bucket from './Bucket';
 
 const BucketList = props => {
-  const { bucketList } = props;
-  const arrMap = bucketList.map(ele => <Bucket key={ele.id} bucket={ele} />);
+  const { bucketList, bucketListLoad } = props;
+  const arrMap = bucketList.map(ele => (
+    <Bucket key={ele.id} bucket={ele} bucketListLoad={bucketListLoad} />
+  ));
 
   return <div>{arrMap}</div>;
 };
 
 BucketList.defaultProps = {
   bucketList: [],
+  bucketListLoad: true,
 };
 
 BucketList.propTypes = {
   bucketList: PropTypes.arrayOf(PropTypes.object),
+  bucketListLoad: PropTypes.bool,
 };
 
 export default BucketList;
