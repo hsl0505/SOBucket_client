1b86b3cd637a44ae7a8890f9c91e9e0afa83a222
hyunsung <hslgood@gmail.com>
2020-01-16 21:37:28 +0900

    add islogin props for logout btn

--- src/pages/Home/MainTop.js
+++ src/pages/Home/MainTop.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import MainSearch from './MainSearch';
 import MainNav from './MainNav';
 
-export default class MainTop extends Component {
-  render() {
-    return (
-      <div>
-        <MainSearch />
-        <MainNav />
-        <div className="mainImg">img</div>
-      </div>
-    );
-  }
+export default function MainTop(props) {
+  const { isLogin } = props;
+  return (
+    <div>
+      <MainSearch />
+      <MainNav isLogin={isLogin} />
+      <div className="mainImg">img</div>
+    </div>
+  );
 }
+
+MainTop.defaultProps = {
+  isLogin: false,
+};
+
+MainTop.propTypes = {
+  isLogin: PropTypes.bool,
+};
