578566bbf71594b0b3084cc103d9fc4c7c3a2066
hyunsung <hslgood@gmail.com>
2020-01-18 23:59:53 +0900

    add getBucket component

---
+++ src/pages/Home/GetBucket.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Icon } from 'antd';
+
+export default function GetBucket() {
+  return (
+    <div className="getBucketBtn">
+      <Icon type="pushpin" className="getBucket_icon" />
+    </div>
+  );
+}
