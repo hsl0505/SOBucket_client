fea7f53e46d810fa582e83fd731315f5b5b7724c
hyunsung <hslgood@gmail.com>
2020-01-19 18:24:54 +0900

    refactor mainSearch component for router

--- src/pages/Home/MainSearch.js
+++ src/pages/Home/MainSearch.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+/* eslint-disable react/forbid-prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 // import 'antd/dist/antd.css';
 import { Input } from 'antd';
 
 const { Search } = Input;
 
-export default class MainSearch extends Component {
-  render() {
-    return (
-      <div className="mainSearch">
-        <Search
-          placeholder="검색어를 입력하세요"
-          enterButton
-          size="large"
-          className="mainSearch_search"
-        />
-      </div>
-    );
-  }
+function MainSearch(props) {
+  const { searchClick } = props;
+  const { history } = props;
+  return (
+    <div className="mainSearch">
+      <Search
+        className="mainSearch_search"
+        placeholder="검색어를 입력하세요"
+        enterButton
+        size="large"
+        onSearch={value => {
+          searchClick(value);
+          history.replace('/');
+        }}
+      />
+    </div>
+  );
 }
+
+MainSearch.defaultProps = {
+  searchClick: () => {},
+  history: '',
+};
+
+MainSearch.propTypes = {
+  searchClick: PropTypes.func,
+  history: PropTypes.any,
+};
+
+export default withRouter(MainSearch);
