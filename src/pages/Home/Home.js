edfb58fa52aa1e1cd0476f5f94faf0cd9c138dd7
hyunsung <hslgood@gmail.com>
2020-01-20 13:16:34 +0900

    remove fake data for test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,49 +10,49 @@ export default class Home extends Component {
     const { isLogin } = this.props;
     this.state = {
       isLogin,
-      // bucketList: null,
-      bucketListLoad: false, // true는 로딩상태
+      bucketList: [],
+      bucketListLoad: true, // true는 로딩상태
       // fake data
-      bucketList: [
-        { id: 0, userName: '임현성', title: 'test', like: 5, mylike: true },
-        { id: 1, userName: '임현성', title: 'test', like: 5, mylike: true },
-        { id: 2, userName: '임현성', title: 'test', like: 5, mylike: false },
-        { id: 3, userName: '임현성', title: 'test', like: 5, mylike: true },
-        { id: 4, userName: '임현성', title: 'test', like: 5, mylike: false },
-        { id: 5, userName: '임현성', title: 'test', like: 5, mylike: false },
-        { id: 6, userName: '임현성', title: 'test', like: 5, mylike: false },
-        { id: 7, userName: '임현성', title: 'test', like: 5, mylike: false },
-      ],
-      // todayBucketList : null,
-      todayBucketListLoad: false, // true는 로딩상태
+      // bucketList: [
+      //   { id: 0, userName: '임현성', title: 'test', like: 5, mylike: true },
+      //   { id: 1, userName: '임현성', title: 'test', like: 5, mylike: true },
+      //   { id: 2, userName: '임현성', title: 'test', like: 5, mylike: false },
+      //   { id: 3, userName: '임현성', title: 'test', like: 5, mylike: true },
+      //   { id: 4, userName: '임현성', title: 'test', like: 5, mylike: false },
+      //   { id: 5, userName: '임현성', title: 'test', like: 5, mylike: false },
+      //   { id: 6, userName: '임현성', title: 'test', like: 5, mylike: false },
+      //   { id: 7, userName: '임현성', title: 'test', like: 5, mylike: false },
+      // ],
+      todayBucketList: [],
+      todayBucketListLoad: true, // true는 로딩상태
       // fake data
-      todayBucketList: [
-        { id: 0, userName: '임현성', title: 'test', like: 5 },
-        { id: 1, userName: '임현성', title: 'test', like: 10 },
-        { id: 2, userName: '임현성', title: 'test', like: 2 },
-        { id: 3, userName: '임현성', title: 'test', like: 8 },
-      ],
+      // todayBucketList: [
+      //   { id: 0, userName: '임현성', title: 'test', like: 5 },
+      //   { id: 1, userName: '임현성', title: 'test', like: 10 },
+      //   { id: 2, userName: '임현성', title: 'test', like: 2 },
+      //   { id: 3, userName: '임현성', title: 'test', like: 8 },
+      // ],
     };
   }
 
   componentDidMount() {
-    // fetch('http://localhost:3001/buckets/home', {
-    //   method: 'GET',
-    // })
-    //   .then(res => res.json())
-    //   .then(result => {
-    //     this.setState({ bucketListLoad: false, bucketList: result.bucketList });
-    //   });
-    // fetch('http://localhost:3001/buckets/home/today', {
-    //   method: 'GET',
-    // })
-    //   .then(res => res.json())
-    //   .then(result => {
-    //     this.setState({
-    //       todayBucketListLoad: false,
-    //       todayBucketList: result.todayBucketList,
-    //     });
-    //   });
+    fetch('http://localhost:3001/buckets/home', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({ bucketListLoad: false, bucketList: result.bucketList });
+      });
+    fetch('http://localhost:3001/buckets/home/today', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          todayBucketListLoad: false,
+          todayBucketList: result.todayBucketList,
+        });
+      });
   }
 
   render() {
