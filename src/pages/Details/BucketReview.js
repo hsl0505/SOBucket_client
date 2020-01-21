a7bb984a7bf0f7c2acd50d40dbef2fb80498cefd
서지훈 <jihunsuh12@gmail.com>
2020-01-21 16:52:10 +0900

    my page comment line little css add

--- src/pages/Details/BucketReview.js
+++ src/pages/Details/BucketReview.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Input, Divider } from 'antd';
 
 export default function BucketReview() {
   return (
-    <div>
-      <input placeholder="title" />
-      <input placeholder="body" />
-      <div>사진인풋</div>
+    <div className="bucketReview">
+      <Divider orientation="left" style={{ fontSize: '24px' }}>
+        댓글
+      </Divider>
+      <Input placeholder="title" />
+      <Input placeholder="body" />
       <button type="button">저장 - 수정</button>
     </div>
   );
