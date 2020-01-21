cf907c3e16b6b372ef5b434f703da496703fc69b
hyunsung <hslgood@gmail.com>
2020-01-21 12:33:14 +0900

    add login handle

--- src/pages/Login/LoginModalButton.js
+++ src/pages/Login/LoginModalButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'antd';
+import PropTypes from 'prop-types';
 import LoginModal from './LoginModal';
 
 export default class LoginModalButton extends React.Component {
@@ -35,6 +36,7 @@ export default class LoginModalButton extends React.Component {
   render() {
     const { handleCancel, showModal, hideModal } = this;
     const { visible } = this.state;
+    const { loginHandle } = this.props;
     return (
       <div>
         <Button
@@ -49,8 +51,17 @@ export default class LoginModalButton extends React.Component {
           hideModal={hideModal}
           visible={visible}
           handleCancel={handleCancel}
+          loginHandle={loginHandle}
         />
       </div>
     );
   }
 }
+
+LoginModalButton.defaultProps = {
+  loginHandle: () => {},
+};
+
+LoginModalButton.propTypes = {
+  loginHandle: PropTypes.func,
+};
